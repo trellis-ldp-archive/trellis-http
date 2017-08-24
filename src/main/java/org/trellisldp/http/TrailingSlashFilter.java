ec323274c8451957750ff712670e7c8d63f91b12
Aaron Coburn <acoburn@amherst.edu>
2017-08-24 10:09:02 -0400

    Update gradle

--- src/main/java/org/trellisldp/http/TrailingSlashFilter.java
+++ src/main/java/org/trellisldp/http/TrailingSlashFilter.java
@@ -32,6 +32,7 @@ public class TrailingSlashFilter implements ContainerRequestFilter {
     public void filter(final ContainerRequestContext ctx) throws IOException {
         final String path = ctx.getUriInfo().getPath();
         if (path.endsWith("/")) {
+            System.out.println("Redirecting: " + path);
             ctx.abortWith(seeOther(fromUri(path.substring(0, path.length() - 1)).build()).build());
         }
     }
