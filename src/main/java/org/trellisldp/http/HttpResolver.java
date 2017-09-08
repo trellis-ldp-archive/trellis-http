f584a345d181a50739a83b2fc291c2e19d8ef0cf
Aaron Coburn <acoburn@amherst.edu>
2017-09-08 09:54:28 -0400

    Add HTTP binary resolver, using the JAX-RS client

---
+++ src/main/java/org/trellisldp/http/HttpResolver.java
@@ -0,0 +1,162 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.trellisldp.http;
+
+import static java.util.Arrays.asList;
+import static java.util.Objects.requireNonNull;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static java.util.Optional.ofNullable;
+import static javax.ws.rs.client.ClientBuilder.newClient;
+import static javax.ws.rs.client.Entity.entity;
+import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
+import static javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM_TYPE;
+import static javax.ws.rs.core.Response.Status.Family.SUCCESSFUL;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.rdf.api.IRI;
+import org.slf4j.Logger;
+import org.trellisldp.spi.BinaryService;
+import org.trellisldp.spi.BinaryService.MultipartUpload;
+import org.trellisldp.spi.RuntimeRepositoryException;
+
+/**
+ * @author acoburn
+ */
+public class HttpResolver implements BinaryService.Resolver {
+
+    private static final String HTTP_RESOLVER_NO_MULTIPART = "HTTP Resolver does not support multipart uploads";
+    private static final String NON_NULL_IDENTIFIER = "Identifier may not be null!";
+
+    private static final Logger LOGGER = getLogger(HttpResolver.class);
+
+    private final Client httpClient;
+
+    /**
+     * Create an HttpResolver using the default HTTP client
+     */
+    public HttpResolver() {
+        this(newClient());
+    }
+
+    /**
+     * Create an HttpResolver with a provided client
+     * @param client the client
+     */
+    public HttpResolver(final Client client) {
+        requireNonNull(client, "HTTP client may not be null!");
+        httpClient = client;
+    }
+
+    @Override
+    public List<String> getUriSchemes() {
+        return asList("http", "https");
+    }
+
+    @Override
+    public Boolean supportsMultipartUpload() {
+        return false;
+    }
+
+    @Override
+    public String initiateUpload(final String partition, final IRI identifier, final String mimeType) {
+        throw new UnsupportedOperationException(HTTP_RESOLVER_NO_MULTIPART);
+    }
+
+    @Override
+    public String uploadPart(final String identifier, final Integer partNumber, final InputStream content) {
+        throw new UnsupportedOperationException(HTTP_RESOLVER_NO_MULTIPART);
+    }
+
+    @Override
+    public MultipartUpload completeUpload(final String identifier, final Map<Integer, String> partDigests) {
+        throw new UnsupportedOperationException(HTTP_RESOLVER_NO_MULTIPART);
+    }
+
+    @Override
+    public void abortUpload(final String identifier) {
+        throw new UnsupportedOperationException(HTTP_RESOLVER_NO_MULTIPART);
+    }
+
+    @Override
+    public Stream<Map.Entry<Integer, String>> listParts(final String identifier) {
+        throw new UnsupportedOperationException(HTTP_RESOLVER_NO_MULTIPART);
+    }
+
+    @Override
+    public Boolean uploadSessionExists(final String identifier) {
+        throw new UnsupportedOperationException(HTTP_RESOLVER_NO_MULTIPART);
+    }
+
+    @Override
+    public Boolean exists(final String partition, final IRI identifier) {
+        requireNonNull(identifier, NON_NULL_IDENTIFIER);
+        final Response res = httpClient.target(identifier.getIRIString()).request().head();
+        final Boolean status = res.getStatusInfo().getFamily().equals(SUCCESSFUL);
+        LOGGER.info("HTTP HEAD request to {} returned status {}", identifier, res.getStatus());
+        res.close();
+        return status;
+    }
+
+    @Override
+    public Optional<InputStream> getContent(final String partition, final IRI identifier) {
+        requireNonNull(identifier,  NON_NULL_IDENTIFIER);
+        final Response res = httpClient.target(identifier.getIRIString()).request().get();
+        LOGGER.info("HTTP GET request to {} returned status {}", identifier, res.getStatus());
+        if (res.hasEntity()) {
+            return of(res.getEntity()).map(x -> (InputStream) x);
+        }
+        return empty();
+    }
+
+    @Override
+    public void setContent(final String partition, final IRI identifier, final InputStream stream,
+            final Map<String, String> metadata) {
+        requireNonNull(identifier, NON_NULL_IDENTIFIER);
+        final Response res = httpClient.target(identifier.getIRIString()).request().put(entity(stream,
+                    ofNullable(metadata.get(CONTENT_TYPE)).map(MediaType::valueOf)
+                        .orElse(APPLICATION_OCTET_STREAM_TYPE)));
+        LOGGER.info("HTTP PUT request to {} returned {}", identifier, res.getStatusInfo());
+        final Boolean ok = res.getStatusInfo().getFamily().equals(SUCCESSFUL);
+        res.close();
+        if (!ok) {
+            throw new RuntimeRepositoryException("HTTP PUT request to " + identifier + " failed with a " +
+                    res.getStatusInfo());
+        }
+    }
+
+    @Override
+    public void purgeContent(final String partition, final IRI identifier) {
+        requireNonNull(identifier, NON_NULL_IDENTIFIER);
+        final Response res = httpClient.target(identifier.getIRIString()).request().delete();
+        LOGGER.info("HTTP DELETE request to {} returned {}", identifier, res.getStatusInfo());
+        final Boolean ok = res.getStatusInfo().getFamily().equals(SUCCESSFUL);
+        res.close();
+        if (!ok) {
+            throw new RuntimeRepositoryException("HTTP DELETE request to " + identifier + " failed with a " +
+                    res.getStatusInfo());
+
+        }
+    }
+}
---
+++ src/test/java/org/trellisldp/http/HttpResolverTest.java
@@ -0,0 +1,245 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.trellisldp.http;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Collections.emptyMap;
+import static javax.ws.rs.core.Response.Status.Family.CLIENT_ERROR;
+import static javax.ws.rs.core.Response.Status.Family.SUCCESSFUL;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.RDF;
+import org.apache.commons.rdf.simple.SimpleRDF;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.trellisldp.spi.BinaryService.Resolver;
+import org.trellisldp.spi.RuntimeRepositoryException;
+
+/**
+ * @author acoburn
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class HttpResolverTest {
+
+    private final static RDF rdf = new SimpleRDF();
+
+    private final static IRI resource = rdf.createIRI("http://www.trellisldp.org/ns/trellis.ttl");
+    private final static IRI sslResource = rdf.createIRI("https://acdc.amherst.edu/ontology/relationships.rdf");
+    private final static String partition = "partition";
+
+    @Mock
+    private Client mockClient;
+
+    @Mock
+    private Response mockResponse;
+
+    @Mock
+    private WebTarget mockWebTarget;
+
+    @Mock
+    private Invocation.Builder mockInvocationBuilder;
+
+    @Mock
+    private Response.StatusType mockStatusType;
+
+    @Before
+    public void setUp() throws IOException {
+        when(mockClient.target(anyString())).thenReturn(mockWebTarget);
+        when(mockWebTarget.request()).thenReturn(mockInvocationBuilder);
+        when(mockInvocationBuilder.put(any(Entity.class))).thenReturn(mockResponse);
+        when(mockInvocationBuilder.delete()).thenReturn(mockResponse);
+        when(mockInvocationBuilder.get()).thenReturn(mockResponse);
+        when(mockResponse.getStatusInfo()).thenReturn(mockStatusType);
+        when(mockStatusType.getFamily()).thenReturn(SUCCESSFUL);
+        when(mockStatusType.toString()).thenReturn("Successful");
+    }
+
+    @Test
+    public void testExists() {
+
+        final Resolver resolver = new HttpResolver();
+
+        assertTrue(resolver.exists(partition, resource));
+        assertTrue(resolver.exists(partition, sslResource));
+        assertFalse(resolver.exists(partition, rdf.createIRI("http://acdc.amherst.edu/ontology/foo.bar")));
+    }
+
+    @Test
+    public void testGetContent() {
+        final Resolver resolver = new HttpResolver();
+
+        assertTrue(resolver.getContent(partition, resource).isPresent());
+        assertTrue(resolver.getContent(partition, resource).map(this::uncheckedToString).get()
+                .contains("owl:Ontology"));
+    }
+
+    @Test
+    public void testGetSslContent() {
+        final Resolver resolver = new HttpResolver();
+
+        assertTrue(resolver.getContent(partition, sslResource).isPresent());
+        assertTrue(resolver.getContent(partition, sslResource).map(this::uncheckedToString).get()
+                .contains("owl:Ontology"));
+    }
+
+    @Test(expected = RuntimeRepositoryException.class)
+    public void testSetContent() {
+        final String contents = "A new resource";
+        final Resolver resolver = new HttpResolver();
+
+        final InputStream inputStream = new ByteArrayInputStream(contents.getBytes(UTF_8));
+        resolver.setContent(partition, sslResource, inputStream);
+    }
+
+    @Test
+    public void testMockedClient() throws IOException {
+        final Resolver resolver = new HttpResolver(mockClient);
+        final String contents = "A new resource";
+        final InputStream inputStream = new ByteArrayInputStream(contents.getBytes(UTF_8));
+        resolver.setContent(partition, sslResource, inputStream);
+
+        verify(mockInvocationBuilder).put(any(Entity.class));
+    }
+
+    @Test
+    public void testMockedDelete() throws IOException {
+        final Resolver resolver = new HttpResolver(mockClient);
+        resolver.purgeContent(partition, sslResource);
+
+        verify(mockInvocationBuilder).delete();
+    }
+
+    @Test(expected = RuntimeRepositoryException.class)
+    public void testMockedDeleteiException() {
+        when(mockStatusType.getFamily()).thenReturn(CLIENT_ERROR);
+        when(mockStatusType.toString()).thenReturn("BAD REQUEST");
+        final Resolver resolver = new HttpResolver(mockClient);
+        resolver.purgeContent(partition, sslResource);
+    }
+
+    @Test
+    public void testHttpSchemes() {
+        final Resolver resolver = new HttpResolver();
+        assertEquals(2L, resolver.getUriSchemes().size());
+        assertTrue(resolver.getUriSchemes().contains("http"));
+        assertTrue(resolver.getUriSchemes().contains("https"));
+    }
+
+    @Test
+    public void testMultipart() {
+        final Resolver resolver = new HttpResolver();
+        assertFalse(resolver.supportsMultipartUpload());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testIniateMultipart() {
+        final Resolver resolver = new HttpResolver();
+        resolver.initiateUpload(partition, resource, "text/plain");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testMultipartAbort() {
+        final Resolver resolver = new HttpResolver();
+        resolver.abortUpload("test-identifier");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testMultipartComplete() {
+        final Resolver resolver = new HttpResolver();
+        resolver.completeUpload("test-identifier", emptyMap());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testMultipartUpload() {
+        final String contents = "A new resource";
+        final InputStream inputStream = new ByteArrayInputStream(contents.getBytes(UTF_8));
+        final Resolver resolver = new HttpResolver();
+        resolver.uploadPart("test-identifier", 1, inputStream);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testMultipartSessionExists() {
+        final Resolver resolver = new HttpResolver();
+        resolver.uploadSessionExists("test-identifier");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testMultipartInitiate() {
+        final Resolver resolver = new HttpResolver();
+        resolver.initiateUpload(partition, resource, "text/plain");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testMultipartList() {
+        final Resolver resolver = new HttpResolver();
+        resolver.listParts("foo");
+    }
+
+
+    @Test(expected = RuntimeRepositoryException.class)
+    public void testExceptedPut() throws IOException {
+        when(mockStatusType.getFamily()).thenReturn(CLIENT_ERROR);
+        when(mockStatusType.toString()).thenReturn("BAD REQUEST");
+        final String contents = "A new resource";
+        final Resolver resolver = new HttpResolver(mockClient);
+        final InputStream inputStream = new ByteArrayInputStream(contents.getBytes(UTF_8));
+
+        resolver.setContent(partition, resource, inputStream);
+    }
+
+    @Test(expected = RuntimeRepositoryException.class)
+    public void testExceptedDelete() throws IOException {
+        when(mockStatusType.getFamily()).thenReturn(CLIENT_ERROR);
+        when(mockStatusType.toString()).thenReturn("BAD REQUEST");
+        final Resolver resolver = new HttpResolver(mockClient);
+
+        resolver.purgeContent(partition, resource);
+    }
+
+    @Test
+    public void testGetNoEntity() throws IOException {
+        //when(mockResponse.hasEntity()).thenReturn(false);
+        final Resolver resolver = new HttpResolver(mockClient);
+        assertFalse(resolver.getContent(partition, resource).isPresent());
+    }
+
+    private String uncheckedToString(final InputStream is) {
+        try {
+            return IOUtils.toString(is, UTF_8);
+        } catch (final IOException ex) {
+            return null;
+        }
+    }
+}
